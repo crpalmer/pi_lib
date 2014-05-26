5d56d25b603bb7c9d8863fb7094da3245bd95421
Christopher R. Palmer <crpalmer@gmail.com>
2014-05-26 06:02:20 -0400

    nano_add_usec: Fix numeric overflow

--- time-utils.h
+++ time-utils.h
@@ -4,13 +4,24 @@
 #include <time.h>
 
 #define TIMESPEC_FMT "%d.%09d"
-#define NANOSEC_PER_MS 1000000
-#define MS_PER_SEC     1000
+#define NANOSEC_PER_MS   1000000
+#define USEC_PER_SEC	 1000000
+#define NANOSEC_PER_USEC 1000
+#define MS_PER_SEC       1000
 
 static inline void
 nano_gettime(struct timespec *t)
 {
      clock_gettime(CLOCK_MONOTONIC, t);
+}
+
+static inline void
+nano_add_usec(struct timespec *t, unsigned usec)
+{
+    t->tv_sec += usec / USEC_PER_SEC;
+    t->tv_nsec += (usec % USEC_PER_SEC) * NANOSEC_PER_USEC;
+    t->tv_sec += t->tv_nsec / (MS_PER_SEC*NANOSEC_PER_MS);
+    t->tv_nsec = t->tv_nsec % (MS_PER_SEC*NANOSEC_PER_MS);
 }
 
 static inline void
