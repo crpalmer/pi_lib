74bebfa53b2d7f602ab9d5b4f5a2785a31c2aff2
Christopher R. Palmer <crpalmer@gmail.com>
2015-10-16 20:14:58 -0400

    time-utils: Fix adding a large time unit & add a later_than function

--- time-utils.h
+++ time-utils.h
@@ -2,6 +2,7 @@
 #define __TIME_UTILS_H__
 
 #include <time.h>
+#include <assert.h>
 
 #define TIMESPEC_FMT "%d.%09d"
 #define NANOSEC_PER_MS   1000000
@@ -27,9 +28,40 @@ nano_add_usec(struct timespec *t, unsigned usec)
 static inline void
 nano_add_ms(struct timespec *t, unsigned ms)
 {
-    t->tv_nsec += ms * NANOSEC_PER_MS;
+    t->tv_sec += ms / 1000;
+    t->tv_nsec += (ms % 1000) * NANOSEC_PER_MS;
     t->tv_sec += t->tv_nsec / (MS_PER_SEC*NANOSEC_PER_MS);
     t->tv_nsec = t->tv_nsec % (MS_PER_SEC*NANOSEC_PER_MS);
+}
+
+static inline int
+nano_later_than(struct timespec *now, struct timespec *then)
+{
+    return now->tv_sec > then->tv_sec || (now->tv_sec == then->tv_sec && now->tv_nsec >= then->tv_nsec);
+}
+
+static inline int
+nano_elapsed_ms(struct timespec *newer, struct timespec *later)
+{
+    int ms = (newer->tv_sec - later->tv_sec) * MS_PER_SEC;
+    int nsec;
+
+    if (newer->tv_nsec < later->tv_nsec) {
+	nsec = newer->tv_nsec + (MS_PER_SEC*NANOSEC_PER_MS);
+	ms -= MS_PER_SEC;
+    } else {
+	nsec = newer->tv_nsec;
+    }
+
+    ms += (nsec - later->tv_nsec) / NANOSEC_PER_MS;
+
+#if 0
+    printf("%ld.%010ld - %ld.%010ld = %d * 1000\n", newer->tv_sec, newer->tv_nsec, later->tv_sec, later->tv_nsec, ms);
+#endif
+
+    assert(ms >= 0);
+
+    return ms;
 }
 
 static inline void
