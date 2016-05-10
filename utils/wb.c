030d3ac52624681bab872dfc05487810e62e8103
Christopher R. Palmer <crpalmer@gmail.com>
2016-05-10 16:06:08 -0400

    wb: Use non-geek numbering for command line inputs

--- utils/wb.c
+++ utils/wb.c
@@ -16,19 +16,19 @@ main(int argc, char **argv)
 
 	if (buf[0] == 'g') {
 	    if (sscanf(&buf[1], "%d", &pin) == 1) {
-		printf("%d = %d\n", pin, wb_get(wb, pin));
+		printf("%d = %d\n", pin, wb_get(wb, pin-1));
 	    } else {
 		goto usage;
 	    }
 	} else if (buf[0] == 's') {
 	    if (sscanf(&buf[1], "%d %d %d", &bank, &pin, &value) == 3) {
-		wb_set(wb, WB_OUTPUT(bank, pin), value);
+		wb_set(wb, WB_OUTPUT(bank-1, pin-1), value);
 	    } else {
 		goto usage;
 	    }
 	} else {
 usage:
-	    fprintf(stderr, "g <pin>\ns <bank> <pin> <value>\n");
+	    fprintf(stderr, "g <pin 1-8>\ns <bank 1/2> <pin 1-8> <value 0/1>\n");
 	}
     }
 
