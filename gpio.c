1b80d9d56a1a15834b1b189baadae085a1976fff
Christopher R. Palmer <crpalmer@gmail.com>
2016-06-25 12:02:54 -0400

    gpio: Convert to using pigpio by default

--- gpio.c
+++ gpio.c
@@ -6,6 +6,7 @@
 #include "file.h"
 #include "string-utils.h"
 #include "gpio.h"
+#include "pigpio.h"
 
 #define GPIO_DIR_PREFIX "/sys/class/gpio/gpio"
 #define EXPORT_FNAME "/sys/class/gpio/export"
@@ -16,60 +17,28 @@
 
 struct gpioS {
      gpio_table_t *table;
-     char        **value_fname;
      int           n_table;
 };
 
-static void
-set_gpio_id(gpio_t *g, size_t i, int value)
-{
-    fatal_echo(g->value_fname[i], "%d\n", value);
-}
-
-static bool
-get_gpio_id(gpio_t *g, size_t i)
-{
-    FILE *f = fopen(g->value_fname[i], "r");
-    int value;
-
-    if (! f) {
-	perror(g->value_fname[i]);
-	exit(1);
-    }
-
-    if (fscanf(f, "%d", &value) != 1) {
-	fprintf(stderr, "Failed to read value from %s\n", g->value_fname[i]);
-	exit(1);
-    }
-
-    fclose(f);
-
-    return value != 0;
-}
-    
 gpio_t *
 gpio_new(gpio_table_t *table, int n_table)
 {
      int i;
-     gpio_t *g = fatal_malloc(sizeof(*g));
+     gpio_t *g;
 
+     if (gpioInitialise() < 0) return NULL;
+
+     g = fatal_malloc(sizeof(*g));
      g->table = table;
      g->n_table = n_table;
-     g->value_fname = fatal_malloc(sizeof(*g->value_fname) * g->n_table);
 
      for (i = 0; i < g->n_table; i++) {
-	char *direction = maprintf("%s%d/direction", GPIO_DIR_PREFIX, g->table[i].gpio);
-	g->value_fname[i] = maprintf("%s%d/value", GPIO_DIR_PREFIX, g->table[i].gpio);
-
-	fatal_echo(EXPORT_FNAME, "%d\n", g->table[i].gpio);
-
 	if (g->table[i].initially_high == GPIO_IS_INPUT) {
-	    fatal_echo(direction, "in\n");
+	    gpioSetMode(g->table[i].gpio, PI_INPUT);
 	} else {
-	    fatal_echo(direction, "out\n");
-	    set_gpio_id(g, i, g->table[i].initially_high ? LOW_VALUE : HIGH_VALUE);
+	    gpioSetMode(g->table[i].gpio, PI_OUTPUT);
+	    gpioWrite(g->table[i].gpio, g->table[i].initially_high ? LOW_VALUE : HIGH_VALUE);
 	}
-	free(direction);
      }
 
      return g;
@@ -95,8 +64,7 @@ set_gpio(gpio_t *g, const char *name, int value)
     size_t id;
 
     if (! find_id(g, name, &id)) return -1;
-    set_gpio_id(g, id, value);
-    return 0;
+    return gpioWrite(g->table[id].gpio, value);
 }
 
 int
@@ -114,13 +82,13 @@ gpio_high(gpio_t *g, const char *name)
 void
 gpio_low_id(gpio_t *g, size_t id)
 {
-     set_gpio_id(g, id, LOW_VALUE);
+    gpioWrite(g->table[id].gpio, LOW_VALUE);
 }
 
 void
 gpio_high_id(gpio_t *g, size_t id)
 {
-     set_gpio_id(g, id, HIGH_VALUE);
+    gpioWrite(g->table[id].gpio, HIGH_VALUE);
 }
 
 bool
@@ -129,25 +97,17 @@ gpio_get(gpio_t *g, const char *name)
     size_t id;
 
     if (! find_id(g, name, &id)) return false;
-    return gpio_get_id(g, id);
+    return gpioRead(g->table[id].gpio);
 }
 
 bool
 gpio_get_id(gpio_t *g, unsigned id)
 {
-    return get_gpio_id(g, id);
+    return gpioRead(g->table[id].gpio);
 }
 
 void
 gpio_destroy(gpio_t *g)
 {
-     int i;
-
-     for (i = 0; i < g->n_table; i++) {
-	echo(UNEXPORT_FNAME, "%d\n", g->table[i].gpio);
-	free(g->value_fname[i]);
-     }
-
-     free(g->value_fname);
      free(g);
 }
