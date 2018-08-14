8a59d70762e5edc87c8d3449a7651a3212fbbfa3
Christopher R. Palmer <crpalmer@gmail.com>
2018-08-14 12:03:55 -0400

    wb: Add a background input watcher thread to utils/wb

--- utils/wb.c
+++ utils/wb.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include "time-utils.h"
 #include "wb.h"
 
 static char buf[1024];
+static pthread_t watcher;
+static volatile int watcher_enabled;
+
+#define WATCHER_DEBOUNCE_MS 5
+
+static void
+print_inputs(unsigned value)
+{
+
+    int pin;
+
+    for (pin = 7; pin >= 0; pin--) {
+	printf("%d", (value & (1<<pin)) != 0);
+    }
+}
+
+static void *
+watcher_main(void *unused)
+{
+    unsigned last = wb_get_all_with_debounce(WATCHER_DEBOUNCE_MS);
+    int was_enabled = 0;
+    struct timespec at;
+
+    while (true) {
+	if (watcher_enabled)  {
+	    unsigned cur;
+	    if (! was_enabled) {
+		was_enabled = 1;
+		nano_gettime(&at);
+		last = cur;
+	    } else {
+	        cur = wb_get_all_with_debounce(WATCHER_DEBOUNCE_MS);
+		if (cur != last) {
+		    print_inputs(cur);
+		    printf(" %d\n", nano_elapsed_ms_now(&at));
+		    last = cur;
+		    nano_gettime(&at);
+		}
+	    }
+	}
+    }
+    return NULL;
+}
 
 int
 main(int argc, char **argv)
@@ -12,6 +57,8 @@ main(int argc, char **argv)
 	exit(1);
     }
 
+    pthread_create(&watcher, NULL, watcher_main, NULL);
+
     while (fgets(buf, sizeof(buf), stdin) != NULL && ! feof(stdin)) {
 	int bank, pin, value;
 
@@ -19,10 +66,7 @@ main(int argc, char **argv)
 	    if (sscanf(&buf[1], "%d", &pin) == 1) {
 		printf("%d = %d\n", pin, wb_get(pin));
 	    } else {
-		value = wb_get_all();
-		for (pin = 7; pin >= 0; pin--) {
-		    printf("%d", (value & (1<<pin)) != 0);
-		}
+		print_inputs(wb_get_all());
 		printf("\n");
 	    }
 	} else if (buf[0] == 'G') {
@@ -76,9 +120,11 @@ main(int argc, char **argv)
 		mask = WB_PIN_MASK_ALL;
 	    }
 	    printf("wait got pin %d\n", wb_wait_for_pins(mask, values));
+	} else if (buf[0] == 'T' || buf[0] == 't') {
+	    watcher_enabled = buf[0] == 'T';
 	} else {
 usage:
-	    fprintf(stderr, "g [<pin 1-8>]\np <bank 1/2> <pin 1-8> [<freq>] <duty%%>\ns <bank 1/2> <pin 1-8> <value 0/1>\nv <bank 1/2> <pin 1-8> <servo pulse width in us>- <pin 1-8> set pull down\n+ <pin 1-8> set pull up\n= <pin 1-8> remove pulll up/down\nw [ <pin 1-8> ] wait for a pin to read true\nW [ <pin 1-8> ] wait for a pin to read false\n");
+	    fprintf(stderr, "g [<pin 1-8>]\np <bank 1/2> <pin 1-8> [<freq>] <duty%%>\ns <bank 1/2> <pin 1-8> <value 0/1>\nv <bank 1/2> <pin 1-8> <servo pulse width in us>- <pin 1-8> set pull down\n+ <pin 1-8> set pull up\n= <pin 1-8> remove pulll up/down\nw [ <pin 1-8> ] wait for a pin to read true\nW [ <pin 1-8> ] wait for a pin to read false\nT enable watcher thread\nt disable watcher thread\n");
 	}
     }
 
