343a8618105fb6b8ab2d973e396826f59a8c4b83
Christopher R. Palmer <crpalmer@gmail.com>
2018-09-21 14:32:31 -0400

    digital-counter: Make the util accept pins via cmdline

--- utils/digital-counter.c
+++ utils/digital-counter.c
@@ -9,8 +9,13 @@ int main(int argc, char **argv)
 {
      digital_counter_t *dc;
 
+     if (argc != 4 && argc != 5) {
+	fprintf(stderr, "usage: bank inc dec reset\n");
+	exit(1);
+     }
+
      wb_init();
-     dc = digital_counter_new(2, 2, 3, 1);
+     dc = digital_counter_new(atoi(argv[1]), atoi(argv[2]), argc == 4 ? -1 : atoi(argv[4]), atoi(argv[3]));
 
      while (fgets(line, sizeof(line), stdin) != NULL) {
 	if (line[0] == '=') {
