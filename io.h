0940127e29db25fe46f29f3b9f4225f5f7dc99b6
Christopher R. Palmer <crpalmer@gmail.com>
2019-10-22 11:45:41 -0400

    io: add get_with_debounce

--- io.h
+++ io.h
@@ -1,6 +1,8 @@
 #ifndef __IO_H__
 #define __IO_H__
 
+#include "time-utils.h"
+
 class output_t {
 public:
      ~output_t() {}
@@ -18,6 +20,20 @@ public:
      virtual void set_pullup_down() = 0;
      virtual void clear_pullup() = 0;
      virtual void set_debounce(unsigned ms) { }
+ 
+    unsigned get_with_debounce(unsigned ms = 1) {
+        struct timespec start;
+	unsigned v = get();
+
+        nano_gettime(&start);
+        while (1) {
+	    double vv = get();
+	    if (v != vv) {
+		v = vv;
+		nano_gettime(&start);
+            } else if (nano_elapsed_ms_now(&start) >= (int) ms) return v;
+        }
+    }
 };
 
 #endif
