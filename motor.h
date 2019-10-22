683dca53cf6db5f05fed6e60d6b87f3ad851e9a4
Christopher R. Palmer <crpalmer@gmail.com>
2019-10-22 11:46:13 -0400

    motor: add unified dir/speed change and stop methods

--- motor.h
+++ motor.h
@@ -1,11 +1,27 @@
 #ifndef __MOTOR_H__
 #define __MOTOR_H__
 
+#include "util.h"
+
 class motor_t {
 public:
     ~motor_t() { }
     virtual void speed(double pct) = 0;
     virtual void direction(bool forward) = 0;
+
+    void change_motor(bool direction, double speed)
+    {
+        this->speed(0);
+        ms_sleep(50);
+        this->direction(direction);
+        this->speed(speed);
+    }
+
+    void stop()
+    {
+	this->speed(0);
+	ms_sleep(50);
+    }
 };
 
 #endif
