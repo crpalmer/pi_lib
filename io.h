275a6dffb7cf4389a6460bfa8a9fd693257e7dd6
Christopher R. Palmer <crpalmer@gmail.com>
2022-07-09 07:26:45 -0400

    input_t: allow the user to invert the meaning of the value
    This makes the end code much easier to read and it also allows you
    to switch from NO to NC without having to modify all the code (just
    change the inverted state).

--- io.h
+++ io.h
@@ -14,20 +14,21 @@ public:
 
 class input_t {
 public:
-     input_t() { debounce_ms = 0; }
+     input_t() { debounce_ms = 0; is_inverted = 0; }
      ~input_t() {}
      virtual unsigned get_fast() = 0;
      virtual void set_pullup_up() = 0;
      virtual void set_pullup_down() = 0;
      virtual void clear_pullup() = 0;
 
+     void set_inverted(int set_inverted = true) { is_inverted = set_inverted; }
      void set_debounce(unsigned ms) { debounce_ms = ms; }
  
      unsigned get() {
 	if (debounce_ms) {
 	    return get_with_debounce(debounce_ms);
 	} else {
-	    return get_fast();
+	    return return_value(get_fast());
 	}
      }
 
@@ -41,12 +42,19 @@ public:
 	    if (v != vv) {
 		v = vv;
 		nano_gettime(&start);
-            } else if (nano_elapsed_ms_now(&start) >= (int) ms) return v;
+            } else if (nano_elapsed_ms_now(&start) >= (int) ms) {
+		return return_value(v);
+	    }
         }
     }
 
 protected:
+    unsigned return_value(unsigned v) {
+	return is_inverted ? ! v : v;
+    }
+
     unsigned debounce_ms;
+    int is_inverted;
 };
 
 #endif
