d1ccec9a6a1f23739c90386e5b2616b2d0392e77
Christopher R. Palmer <crpalmer@gmail.com>
2023-07-23 15:50:20 -0400

    utils: neopixel-pico: Add fade in/out and make set_all show the leds

--- utils/neopixel-pico.cpp
+++ utils/neopixel-pico.cpp
@@ -10,18 +10,73 @@
 
 static char line[100*1024];
 
+static bool echo = true;
+
 #define STRNCMP(a, b) strncmp(a, b, strlen(b))
+
+#define FADE_INC	1
+#define FADE_SLEEP_MS	10
+
+static bool
+get_rgb(const char *usage, const char *line, int *r, int *g, int *b)
+{
+    if (sscanf(line, "%d %d %d", r, g, b) == 3) {
+	return true;
+    }
+    printf("%s <r> <g> <b>\n", usage);
+    return false;
+}
+
+static void
+show(NeoPixelPico *neo)
+{
+    struct timespec start;
+
+    if (echo) nano_gettime(&start);
+    neo->show();
+    if (echo) printf("show took %d ms\n", nano_elapsed_ms_now(&start));
+}
+
+static void
+fade_in(NeoPixelPico *neo, int final_r, int final_g, int final_b)
+{
+    int r = 0, g = 0, b = 0;
+
+    while (r < final_r || g < final_g || b < final_b) {
+	if (r < final_r) r += FADE_INC;
+	if (g < final_g) g += FADE_INC;
+	if (b < final_b) b += FADE_INC;
+	neo->set_all(r, g, b);
+	neo->show();
+	ms_sleep(FADE_SLEEP_MS);
+    }
+    printf("faded in to %d,%d,%d\n", r, g, b);
+}
+
+static void
+fade_out(NeoPixelPico *neo, int r, int g, int b)
+{
+    while (r > 0 || g > 0 || b > 0) {
+	if (r > 0) r -= FADE_INC;
+	if (g > 0) g -= FADE_INC;
+	if (b > 0) b -= FADE_INC;
+	neo->set_all(r, g, b);
+	neo->show();
+	ms_sleep(FADE_SLEEP_MS);
+    }
+    printf("fade out complete.\n");
+}
 
 int
 main()
 {
-    bool echo = false;
-
     pi_init_no_reboot();
 
     NeoPixelPico *neo = new NeoPixelPico(0);
 
     for (;;) {
+	int r, g, b;
+
 	while (!tud_cdc_connected()) {
 	    ms_sleep(1);
 	}
@@ -43,14 +98,19 @@ main()
 	    echo = atoi(&line[space]);
 	} else if (STRNCMP(line, "set_brightness ") == 0) {
 	    neo->set_brightness(atof(&line[space]));
+	} else if (STRNCMP(line, "fade_in ") == 0) {
+	    if (get_rgb("fade_in: usage", &line[space], &r, &g, &b)) {
+		fade_in(neo, r, g, b);
+	    }
+	} else if (STRNCMP(line, "fade_out ") == 0) {
+	    if (get_rgb("fade_in: usage", &line[space], &r, &g, &b)) {
+		fade_out(neo, r, g, b);
+	    }
 	} else if (STRNCMP(line, "set_all ") == 0) {
-	    int r, g, b;
-
-	    if (sscanf(&line[space], "%d %d %d", &r, &g, &b) == 3) {
+	    if (get_rgb("set_all: usage", &line[space], &r, &g, &b)) {
 	        neo->set_all(r, g, b);
+		show(neo);
 		if (echo) printf("set_all to %d,%d,%d\n", r, g, b);
-	    } else if (echo) {
-		printf("set_all: usage <r> <g> <b>\n");
 	    }
 	} else if (STRNCMP(line, "set_led ") == 0) {
 	    int led, r, g, b;
@@ -64,15 +124,13 @@ main()
 	} else if (STRNCMP(line, "set_n_leds ") == 0) {
 	    neo->set_n_leds(atoi(&line[space]));
 	} else if (strcmp(line, "show") == 0) {
-	    struct timespec start;
-
-	    if (echo) nano_gettime(&start);
-	    neo->show();
-	    if (echo) printf("show took %d ms\n", nano_elapsed_ms_now(&start));
+	    show(neo);
 	} else if (strcmp(line, "help") == 0) {
 	    printf("bootsel: reboot into bootloader mode\n");
 	    printf("dump\n");
 	    printf("echo <0/1>: set echo mode\n");
+	    printf("fade_in <r> <g> <b>\n");
+	    printf("fade_out <r> <g> <b>\n");
 	    printf("set_all <r> <g> <b>\n");
 	    printf("set_brightness <pct>\n");
 	    printf("set_led <led> <r> <g> <b>\n");
