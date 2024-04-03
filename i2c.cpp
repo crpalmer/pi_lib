f1744ab70cc789c0daab2e210a09e1cb2cf356f6
Christopher R. Palmer <crpalmer@gmail.com>
2024-04-03 16:13:51 +0000

    fixup: missing i2c files!

---
+++ i2c.cpp
@@ -0,0 +1,101 @@
+#include "i2c.h"
+
+#ifdef PI_PICO
+
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+#define MAX_I2C  32
+
+typedef struct {
+    unsigned char bus;
+    unsigned char addr;
+} i2c_data_t;
+
+static i2c_data_t i2c_data[MAX_I2C];
+
+static inline i2c_inst_t *i2c_inst(int bus) { return bus ? i2c1 : i2c0; }
+
+int i2c_init_bus(int bus, int speed)
+{
+    return i2c_init(i2c_inst(bus), speed);
+}
+
+void i2c_config_gpios(int sda, int scl)
+{
+    gpio_set_function(sda, GPIO_FUNC_I2C);
+    gpio_set_function(scl, GPIO_FUNC_I2C);
+}
+
+int i2c_open(int bus, int addr)
+{
+    int fd = 0;
+
+    while (fd < MAX_I2C && i2c_data[fd].addr == 0) {}
+    if (fd >= MAX_I2C) return -1;
+
+    i2c_data[fd].bus = bus;
+    i2c_data[fd].addr = addr;
+
+    return fd;
+}
+
+void i2c_close(int fd)
+{
+    i2c_data[fd].addr = 0;
+}
+
+int i2c_read(int fd, unsigned char reg, void *data, int n_bytes)
+{
+    i2c_data_t *i2c = &i2c_data[fd];
+
+    i2c_write_blocking(i2c_inst(i2c->bus), i2c->addr, &reg, 1, false);
+    return i2c_read_blocking(i2c_inst(i2c->bus), i2c->addr, (uint8_t*) data, n_bytes, false);
+}
+
+int i2c_write(int fd, unsigned char reg, const void *data, int n_bytes)
+{
+    unsigned char msg[n_bytes+1];
+    i2c_data_t *i2c = &i2c_data[fd];
+
+    msg[0] = reg;
+    for (int i = 0; i < n_bytes; i++) {
+	msg[i+1] = ((const unsigned char *) data)[i];
+    }
+    return i2c_write_blocking(i2c_inst(i2c->bus), i2c->addr, msg, n_bytes+1, false);
+}
+
+#else
+
+/* TODO: Get rid of the pigpio layer and use open / ioctl / read / write */
+
+void i2c_init(int speed)
+{
+}
+
+void i2c_config_gpios(int sda, int scl)
+{
+}
+
+inline int i2c_open(int bus, unsigned addr)
+{
+    return i2cOpen(bus, addr, 0);
+}
+
+void i2c_close(int fd)
+{
+    i2cClose(fd);
+}
+
+int i2c_read(int fd, int reg, unsigned char *data, int n_bytes)
+{
+    return i2cReadBlockData(fd, reg, data, n_bytes);
+}
+
+int i2c_write(int fd, int reg, unsigned char *data, int n_bytes)
+{
+    return i2cWriteBlockData(fd, reg, data, n_bytes);
+}
+
+
+#endif
---
+++ i2c.h
@@ -0,0 +1,33 @@
+#ifndef __I2C_H__
+#define __I2C_H__
+
+/* init_bus and config_gpios are no-ops on the PI (only for the pico) */
+int i2c_init_bus(int bus, int bus_speed = 100*1000);
+void i2c_config_gpios(int sda, int scl);
+
+int i2c_open(int bus, int addr);
+void i2c_close(int fd);
+int i2c_read(int fd, unsigned char reg, void *data, int n_bytes);
+int i2c_write(int fd, unsigned char reg, const void *data, int n_bytes);
+
+static inline int i2c_read_byte(int fd, unsigned char reg, unsigned char *data)
+{
+    return i2c_read(fd, reg, data, 1);
+}
+
+static inline int i2c_write_byte(int fd, unsigned char reg, unsigned char data)
+{
+    return i2c_write(fd, reg, &data, 1);
+}
+
+static inline int i2c_read_word(int fd, unsigned char reg, unsigned short *data)
+{
+    return i2c_read(fd, reg, data, 2);
+}
+
+static inline int i2c_write_word(int fd, unsigned char reg, unsigned short data)
+{
+    return i2c_write(fd, reg, &data, 2);
+}
+
+#endif
