95a9c55dec84d4609effda93a2b710be89bf41d5
Christopher R. Palmer <crpalmer@gmail.com>
2024-04-09 15:02:58 -0400

    pi-gpio-pico: Add a debounce to the irq events

--- pi-gpio-pico.c
+++ pi-gpio-pico.c
@@ -7,26 +7,43 @@
 
 #define NUM_GPIOS 40
 
-static enum { G_FREE = 0, G_IN, G_OUT, G_PWM } gpios[NUM_GPIOS];
+typedef struct {
+   pi_gpio_irq_handler_t f;
+   void *arg;
+   alarm_id_t alarm_id;
+   unsigned last_event;
+} irq_handler_state_t;
+
+typedef struct {
+    unsigned gpio;
+    enum { G_FREE = 0, G_IN, G_OUT, G_PWM } state;
+    irq_handler_state_t irq_handler;
+} gpio_t;
+
+static gpio_t gpios[NUM_GPIOS];
 
 void pi_gpio_init()
 {
     servo_init();
     servo_clock_auto();
+    for (int i = 0; i < NUM_GPIOS; i++) {
+	gpios[i].gpio = i;
+	gpios[i].irq_handler.alarm_id = -1;
+    }
 }
 
 int pi_gpio_get(unsigned gpio)
 {
-    if (gpios[gpio] == G_FREE) gpios[gpio] = G_IN;
-    assert(gpios[gpio] == G_IN);
+    if (gpios[gpio].state == G_FREE) gpios[gpio].state = G_IN;
+    assert(gpios[gpio].state == G_IN);
 
     return gpio_get(gpio);
 }
 
 int pi_gpio_set(unsigned gpio, uint8_t value)
 {
-    if (gpios[gpio] == G_FREE) gpios[gpio] = G_OUT;
-    assert(gpios[gpio] == G_OUT);
+    if (gpios[gpio].state == G_FREE) gpios[gpio].state = G_OUT;
+    assert(gpios[gpio].state == G_OUT);
 
     gpio_put(gpio, value);
     return 0;
@@ -43,15 +60,11 @@ int pi_gpio_set_direction(unsigned gpio, unsigned mode)
     gpio_init(gpio);
     pi_gpio_set_pullup(gpio, PI_PUD_OFF);
     gpio_set_dir(gpio, mode == PI_OUTPUT);
-    gpios[gpio] = (mode == PI_OUTPUT ? G_OUT : G_IN);
+    gpios[gpio].state = (mode == PI_OUTPUT ? G_OUT : G_IN);
     return 0;
 }
 
 static bool irq_enabled = false;
-static struct {
-    pi_gpio_irq_handler_t f;
-    void *arg;
-} irq_handlers[NUM_GPIOS];
 
 unsigned translate_events(uint32_t events)
 {
@@ -63,21 +76,42 @@ unsigned translate_events(uint32_t events)
     return new_events;
 }
 
+static int64_t gpio_irq_alarm(alarm_id_t id, void *gpio_as_vp)
+{
+    gpio_t *gpio = (gpio_t *) gpio_as_vp;
+    irq_handler_state_t *irq = &gpio->irq_handler;
+
+    irq->alarm_id = -1;
+    irq->f(irq->arg, gpio->gpio, translate_events(irq->last_event));
+    return 0;
+}
+
 static void gpio_callback(uint gpio, uint32_t events) {
-    if (irq_handlers[gpio].f) irq_handlers[gpio].f(irq_handlers[gpio].arg, gpio, translate_events(events));
+    irq_handler_state_t *irq = &gpios[gpio].irq_handler;
+
+    if (irq->f) {
+	if (irq->alarm_id >= 0) cancel_alarm(irq->alarm_id);
+	irq->last_event = events;
+	if ((irq->alarm_id = add_alarm_in_us(1000, gpio_irq_alarm, &gpios[gpio], true)) < 0) {
+	    fprintf(stderr, "pi-gpio irq_handler: Failed to create alarm for debouncing\n");
+            irq->f(irq->arg, gpio, translate_events(events));
+	}
+    }
 }
 
 int pi_gpio_set_irq_handler(unsigned gpio, pi_gpio_irq_handler_t irq_handler, void *irq_handler_arg)
 {
-    irq_handlers[gpio].f = irq_handler;
+    irq_handler_state_t *irq = &gpios[gpio].irq_handler;
+
+    irq->f = irq_handler;
+    irq->arg = irq_handler_arg;
+
     if (! irq_enabled) {
 	gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 	irq_enabled = true;
     } else {
 	gpio_set_irq_enabled(gpio, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
     }
-    irq_handlers[gpio].f = irq_handler;
-    irq_handlers[gpio].arg = irq_handler_arg;
 
     return 0;
 }
