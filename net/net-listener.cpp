cfa0f15dfddf645236cc10091b6d6ed0f933eb18
Christopher R. Palmer <crpalmer@gmail.com>
2024-04-20 07:03:48 -0400

    net: Add a listener object that spawns threads on accepted connection

---
+++ net/net-listener.cpp
@@ -0,0 +1,31 @@
+#include "net.h"
+#include "consoles.h"
+#include "time-utils.h"
+
+#include "net-listener.h"
+
+void NetListener::main() {
+    consoles_write_str("net: Starting.\n");
+
+    while (1) {
+        uint16_t port = 4567;
+
+        consoles_printf("net: Listening on port %u\n", port);
+        int l_fd = net_listen(port);
+
+        while (1) {
+            int fd;
+            struct sockaddr_in client;
+            socklen_t size = sizeof(client);
+
+            consoles_write_str("net: Waiting for connection.\n");
+            if ((fd = accept(l_fd, (struct sockaddr *) &client, &size)) < 0) {
+                perror("net_accept");
+                ms_sleep(5*1000);
+            } else {
+                consoles_printf("connect from host %s, port %hu.\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+		accepted(fd);
+            }
+        }
+    }
+}
---
+++ net/net-listener.h
@@ -0,0 +1,17 @@
+#ifndef __NET_LISTENER_H__
+#define __NET_LISTENER_H__
+
+#include <stdint.h>
+#include "pi-threads.h"
+
+class NetListener : PiThread {
+public:
+    NetListener(uint16_t port) : port(port) {}
+    virtual void accepted(int fd) = 0;
+    void main();
+
+private:
+    uint16_t port;
+};
+
+#endif
