895db2bfe9495d01927dffae8644faca96129ba5
Christopher R. Palmer <crpalmer@gmail.com>
2024-05-01 13:33:20 -0400

    audio-buffer: Don't read byte-by-byte (this is slow on FAT w/ pico)

--- audio/audio-buffer.h
+++ audio/audio-buffer.h
@@ -6,21 +6,30 @@ class AudioBuffer;
 #include <stdint.h>
 #include "audio.h"
 #include "buffer.h"
+#include "mem.h"
 
 class AudioBuffer : public AudioConfig {
 public:
-    AudioBuffer(Buffer *buffer, AudioConfig *config) : buffer(buffer), config(config) {
+    AudioBuffer(Buffer *buffer, AudioConfig *config, int a_block = 4096) : buffer(buffer), config(config), a_block(a_block) {
 	bytes_per_sample = config->get_bytes_per_sample();
+
+	block = (uint8_t *) fatal_malloc(a_block);
+	block_pos = 0;
+	n_block = 0;
     }
 
     virtual ~AudioBuffer() {
+	free(block);
     }
 
     bool next(uint32_t *val) {
 	*val = 0;
 	for (int i = 0; i < bytes_per_sample; i++) {
-	    if (buffer->is_eof()) return false;
-	    uint8_t byte = buffer->next();
+	    if (block_pos >= n_block) {
+		if ((n_block = buffer->read(block, a_block)) <= 0) return false;
+		block_pos = 0;
+	    }
+	    uint8_t byte = block[block_pos++];
 	    *val = *val | (byte << 8*i);
 	}
 	*val = *val << (8 * (4 - bytes_per_sample));
@@ -35,8 +44,11 @@ public:
 
 private:
     Buffer *buffer;
-    int bytes_per_sample;
     AudioConfig *config;
+
+    uint8_t *block;
+    int a_block, n_block, block_pos;
+    int bytes_per_sample;
 };
 
 #endif
