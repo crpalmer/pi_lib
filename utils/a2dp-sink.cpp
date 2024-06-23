3ad8d4388e8fe3d126ce1342139926b49a328efc
Christopher R. Palmer <crpalmer@gmail.com>
2024-06-23 07:19:11 -0400

    a2dp-sink: wait 1sec to let stdout initialize

--- utils/a2dp-sink.cpp
+++ utils/a2dp-sink.cpp
@@ -1,9 +1,11 @@
 #include "pi.h"
 #include "pi-threads.h"
+#include "time-utils.h"
 
 int btstack_main(int argc, const char * argv[]);
 
 void thread_main(int argc, char **argv) {
+    ms_sleep(1000);
     btstack_main(0, NULL);
     while (1) ms_sleep(1000000);
 }
